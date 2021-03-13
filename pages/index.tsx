dd3e07d7da16937007054b8f9ea6ddf15f084a86
masaphil <yuckyphilia@gmail.com>
2021-03-13 22:52:46 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { GetStaticProps} from 'next';
+
+export default function Blog({ posts }) {
+  return (
+    <div>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <Link href={`blog/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const blog = await fetch('https://philitanblog.microcms.io/api/v1/blog', key)
+  .then(res => res.json())
+  .catch(() => null);
+  return {
+    props: {
+      posts: blog.contents,
+    },
+  };
+};
